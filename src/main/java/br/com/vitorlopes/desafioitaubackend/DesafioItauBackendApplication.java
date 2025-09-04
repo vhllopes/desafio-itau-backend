6e85827197efbacf800db2c24b15f510f9bdee8a
vitorlopes <vitorhllopes18@gmail.com>
2025-09-03 21:06:44 -0300

    Configuração inicial do projeto

---
+++ src/main/java/br/com/vitorlopes/desafioitaubackend/DesafioItauBackendApplication.java
@@ -0,0 +1,13 @@
+package br.com.vitorlopes.desafioitaubackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DesafioItauBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DesafioItauBackendApplication.class, args);
+    }
+
+}
---
+++ src/test/java/br/com/vitorlopes/desafioitaubackend/DesafioItauBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.vitorlopes.desafioitaubackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DesafioItauBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
